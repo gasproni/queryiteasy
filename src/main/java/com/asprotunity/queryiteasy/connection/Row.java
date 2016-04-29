981aa969c1824f9e7e240383c09383c89cfaf4fe
Giovanni Asproni <gasproni@asprotunity.com>
2016-04-29 12:31:25 +0100

    Added methods to Row interface to query value by position.

--- src/main/java/com/asprotunity/queryiteasy/connection/Row.java
+++ src/main/java/com/asprotunity/queryiteasy/connection/Row.java
@@ -11,17 +11,31 @@ import java.util.function.Function;
 
 public interface Row {
     String asString(String columnName);
+    String asString(int position);
     Short asShort(String columnName);
+    Short asShort(int position);
     Integer asInteger(String columnName);
+    Integer asInteger(int position);
     Long asLong(String columnName);
+    Long asLong(int position);
     Double asDouble(String columnName);
+    Double asDouble(int position);
     Float asFloat(String columnName);
+    Float asFloat(int position);
     Byte asByte(String columnName);
+    Byte asByte(int position);
     BigDecimal asBigDecimal(String columnName);
+    BigDecimal asBigDecimal(int position);
     Boolean asBoolean(String columnName);
+    Boolean asBoolean(int position);
     Date asDate(String columnName);
+    Date asDate(int position);
     Time asTime(String columnName);
+    Time asTime(int position);
     Timestamp asTimestamp(String columnName);
+    Timestamp asTimestamp(int position);
     <ResultType> ResultType fromBlob(String columnName,
                                      Function<Optional<InputStream>, ResultType> blobInputStream);
+    <ResultType> ResultType fromBlob(int position,
+                                     Function<Optional<InputStream>, ResultType> blobReader);
 }
--- src/main/java/com/asprotunity/queryiteasy/internal/connection/RowFromResultSet.java
+++ src/main/java/com/asprotunity/queryiteasy/internal/connection/RowFromResultSet.java
@@ -21,9 +21,9 @@ public class RowFromResultSet implements Row {
             columns = new Object[metadata.getColumnCount()];
             nameToColumn = new HashMap<>();
             for (int columnIndex = 1; columnIndex <= columns.length; ++columnIndex) {
-                int position = columnIndex - 1;
+                int position = columnIndex(columnIndex);
                 columns[position] = rs.getObject(columnIndex);
-                nameToColumn.put(normaliseColumnName(metadata.getColumnLabel(columnIndex)), position);
+                nameToColumn.put(normaliseColumnName(metadata.getColumnLabel(columnIndex)), columnIndex);
             }
 
         } catch (SQLException e) {
@@ -33,79 +33,140 @@ public class RowFromResultSet implements Row {
 
     @Override
     public String asString(String columnName) {
-        return String.valueOf(columns[positionForColumn(columnName)]);
+        return asString(columnPosition(columnName));
+    }
+
+    @Override
+    public String asString(int position) {
+        return String.valueOf(columns[columnIndex(position)]);
     }
 
     @Override
     public Short asShort(String columnName) {
-        Object object = columns[positionForColumn(columnName)];
-        return TypeConverters.convertNumber(object, Short.class, Short::valueOf, Number::shortValue);
+        return asShort(columnPosition(columnName));
+    }
+
+    @Override
+    public Short asShort(int position) {
+        return TypeConverters.convertNumber(columns[columnIndex(position)],
+                Short.class, Short::valueOf, Number::shortValue);
     }
 
     @Override
     public Integer asInteger(String columnName) {
-        Object object = columns[positionForColumn(columnName)];
-        return TypeConverters.convertNumber(object, Integer.class, Integer::valueOf, Number::intValue);
+        return asInteger(columnPosition(columnName));
+    }
+
+    @Override
+    public Integer asInteger(int position) {
+        return TypeConverters.convertNumber(columns[columnIndex(position)],
+                Integer.class, Integer::valueOf, Number::intValue);
     }
 
     @Override
     public Long asLong(String columnName) {
-        Object object = columns[positionForColumn(columnName)];
-        return TypeConverters.convertNumber(object, Long.class, Long::valueOf, Number::longValue);
+        return asLong(columnPosition(columnName));
+    }
+
+    @Override
+    public Long asLong(int position) {
+        return TypeConverters.convertNumber(columns[columnIndex(position)],
+                Long.class, Long::valueOf, Number::longValue);
     }
 
     @Override
     public Double asDouble(String columnName) {
-        Object object = columns[positionForColumn(columnName)];
-        return TypeConverters.convertNumber(object, Double.class, Double::valueOf, Number::doubleValue);
+        return asDouble(columnPosition(columnName));
+    }
+
+    @Override
+    public Double asDouble(int position) {
+        return TypeConverters.convertNumber(columns[columnIndex(position)],
+                Double.class, Double::valueOf, Number::doubleValue);
     }
 
     @Override
     public Float asFloat(String columnName) {
-        Object object = columns[positionForColumn(columnName)];
-        return TypeConverters.convertNumber(object, Float.class, Float::valueOf, Number::floatValue);
+        return asFloat(columnPosition(columnName));
+    }
+
+    @Override
+    public Float asFloat(int position) {
+        return TypeConverters.convertNumber(columns[columnIndex(position)],
+                Float.class, Float::valueOf, Number::floatValue);
     }
 
     @Override
     public Byte asByte(String columnName) {
-        Object object = columns[positionForColumn(columnName)];
-        return TypeConverters.convertNumber(object, Byte.class, Byte::valueOf, Number::byteValue);
+        return asByte(columnPosition(columnName));
+    }
+
+    @Override
+    public Byte asByte(int position) {
+        return TypeConverters.convertNumber(columns[columnIndex(position)],
+                Byte.class, Byte::valueOf, Number::byteValue);
     }
 
     @Override
     public BigDecimal asBigDecimal(String columnName) {
-        Object object = columns[positionForColumn(columnName)];
-        return TypeConverters.toBigDecimal(object);
+        return asBigDecimal(columnPosition(columnName));
+    }
+
+    @Override
+    public BigDecimal asBigDecimal(int position) {
+        return TypeConverters.toBigDecimal(columns[columnIndex(position)]);
     }
 
     @Override
     public Boolean asBoolean(String columnName) {
-        Object object = columns[positionForColumn(columnName)];
-        return TypeConverters.toBoolean(object);
+        return asBoolean(columnPosition(columnName));
+    }
+
+    @Override
+    public Boolean asBoolean(int position) {
+        return TypeConverters.toBoolean(columns[columnIndex(position)]);
     }
 
     @Override
     public Date asDate(String columnName) {
-        Object object = columns[positionForColumn(columnName)];
-        return TypeConverters.toSqlDate(object);
+        return asDate(columnPosition(columnName));
+    }
+
+    @Override
+    public Date asDate(int position) {
+        return TypeConverters.toSqlDate(columns[columnIndex(position)]);
     }
 
     @Override
     public Time asTime(String columnName) {
-        Object object = columns[positionForColumn(columnName)];
-        return TypeConverters.toSqlTime(object);
+        return asTime(columnPosition(columnName));
+    }
+
+    @Override
+    public Time asTime(int position) {
+        return TypeConverters.toSqlTime(columns[columnIndex(position)]);
     }
 
     @Override
     public Timestamp asTimestamp(String columnName) {
-        Object object = columns[positionForColumn(columnName)];
-        return TypeConverters.toSqlTimestamp(object);
+        return asTimestamp(columnPosition(columnName));
+    }
+
+    @Override
+    public Timestamp asTimestamp(int position) {
+        return TypeConverters.toSqlTimestamp(columns[columnIndex(position)]);
     }
 
     @Override
     public <ResultType> ResultType fromBlob(String columnName,
                                             Function<Optional<InputStream>, ResultType> blobReader) {
-        Object object = columns[positionForColumn(columnName)];
+        return fromBlob(columnPosition(columnName), blobReader);
+    }
+
+    @Override
+    public <ResultType> ResultType fromBlob(int position,
+                                            Function<Optional<InputStream>, ResultType> blobReader) {
+        Object object = columns[columnIndex(position)];
         return TypeConverters.fromBlob(object, blobReader);
     }
 
@@ -113,8 +174,12 @@ public class RowFromResultSet implements Row {
         return name.toUpperCase();
     }
 
-    public Integer positionForColumn(String columnName) {
+    private Integer columnPosition(String columnName) {
         return nameToColumn.get(normaliseColumnName(columnName));
+    }
+
+    private int columnIndex(int position) {
+        return position - 1;
     }
 
 }
