cadce26d7c6e1a21627ff703e5594a253eef4df7
Giovanni Asproni <gasproni@asprotunity.com>
2016-07-14 14:11:28 +0100

    Refactored AutoCloseableScopeTest class.

--- src/test/java/com/asprotunity/queryiteasy/scope/AutoCloseableScopeTest.java
+++ src/test/java/com/asprotunity/queryiteasy/scope/AutoCloseableScopeTest.java
@@ -1,115 +1,108 @@
 package com.asprotunity.queryiteasy.scope;
 
 import org.junit.Test;
+import org.mockito.InOrder;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class AutoCloseableScopeTest {
 
-    private static class Disposable {
-        private Exception exception;
-        public int disposeOrder = -1;
-        private static int disposeOrderCounter = 1;
-
-        public Disposable() {
-        }
-
-        public Disposable(Exception exception) {
-            this.exception = exception;
-        }
-
-        public void dispose() throws Exception {
-            if (exception != null) {
-                throw exception;
-            }
-            disposeOrder = disposeOrderCounter;
-            ++disposeOrderCounter;
-        }
-
-        public boolean isDisposed() {
-            return disposeOrder > 0;
-        }
-    }
 
     @Test
-    public void calls_single_registered_close_handler() {
-        Disposable disposable = new Disposable();
-        assertFalse(disposable.isDisposed());
-
-        AutoCloseableScope scope = new AutoCloseableScope();
-        scope.add(disposable::dispose);
-        scope.close();
-
-        assertTrue(disposable.isDisposed());
-    }
-
-    @Test
-    public void calls_registered_close_handlers_in_reverse_registration_order() {
-        Disposable disposable1 = new Disposable();
-        Disposable disposable2 = new Disposable();
-
-        AutoCloseableScope scope = new AutoCloseableScope();
-        scope.add(disposable1::dispose);
-        scope.add(disposable2::dispose);
-        scope.close();
-
-        assertThat(disposable1.disposeOrder, is(greaterThan(disposable2.disposeOrder)));
-    }
-
-    @Test
-    public void works_correctly_when_no_handlers_registered() {
+    public void scope_is_not_closed_after_instatiation() {
         AutoCloseableScope scope = new AutoCloseableScope();
         assertFalse(scope.isClosed());
+    }
+
+    @Test
+    public void closes_scope_correctly_when_no_handlers_registered() {
+        AutoCloseableScope scope = new AutoCloseableScope();
         scope.close();
         assertTrue(scope.isClosed());
     }
 
-
     @Test
-    public void closes_normally_and_throws_runtime_exception_when_handler_throws() {
-        Exception thrownByDisposable = new Exception();
-        Disposable disposable = new Disposable(thrownByDisposable);
+    public void calls_single_registered_close_handler() throws Exception {
+        Closeable closeable = mock(Closeable.class);
 
         AutoCloseableScope scope = new AutoCloseableScope();
-        scope.add(disposable::dispose);
+        scope.add(closeable::close);
+        scope.close();
+
+        verify(closeable, times(1)).close();
+        assertTrue(scope.isClosed());
+
+    }
+
+    @Test
+    public void calls_registered_close_handlers_in_reverse_registration_order() throws Exception {
+        Closeable closeable1 = mock(Closeable.class);
+        Closeable closeable2 = mock(Closeable.class);
+
+        AutoCloseableScope scope = new AutoCloseableScope();
+        scope.add(closeable1::close);
+        scope.add(closeable2::close);
+        scope.close();
+
+        InOrder order = inOrder(closeable1, closeable2);
+        order.verify(closeable2, times(1)).close();
+        order.verify(closeable1, times(1)).close();
+        assertTrue(scope.isClosed());
+    }
+
+    @Test
+    public void closes_normally_and_throws_runtime_exception_when_handler_throws() throws Exception {
+        Exception exceptionFromCloseable = new Exception();
+        Closeable closeable = mock(Closeable.class);
+        doThrow(exceptionFromCloseable).when(closeable).close();
+
+        AutoCloseableScope scope = new AutoCloseableScope();
+        scope.add(closeable::close);
 
         try {
             scope.close();
             fail("DisposerExceptionExpected!");
-        }
-        catch (ScopeException exception) {
+        } catch (ScopeException exception) {
             assertTrue(scope.isClosed());
-            assertThat(exception.getCause(), is(thrownByDisposable));
+            assertThat(exception.getCause(), is(exceptionFromCloseable));
         }
     }
 
     @Test
-    public void calls_all_handlers_and_throws_runtime_exception_wrapping_first_thrown_with_all_others_marked_as_suppressed_when_more_than_one_handler_throws() {
-        Exception disposable1Exception = new Exception();
-        Disposable disposable1 = new Disposable(disposable1Exception);
+    public void calls_all_handlers_and_throws_runtime_exception_wrapping_first_thrown_with_all_others_marked_as_suppressed_when_more_than_one_handler_throws() throws Exception {
+        Closeable closeable1 = mock(Closeable.class);
+        Exception closeable1Exception = new Exception();
+        doThrow(closeable1Exception).when(closeable1).close();
 
-        Disposable nonThrowingDisposable = new Disposable();
+        Closeable closeable2 = mock(Closeable.class);
 
-        Exception disposable2Exception = new Exception();
-        Disposable disposable2 = new Disposable(disposable2Exception);
+        Closeable closeable3 = mock(Closeable.class);
+        Exception closeable3Exception = new Exception();
+        doThrow(closeable3Exception).when(closeable3).close();
 
         AutoCloseableScope scope = new AutoCloseableScope();
-        scope.add(disposable1::dispose);
-        scope.add(nonThrowingDisposable::dispose);
-        scope.add(disposable2::dispose);
+        scope.add(closeable1::close);
+        scope.add(closeable2::close);
+        scope.add(closeable3::close);
 
         try {
             scope.close();
             fail("DisposerExceptionExpected!");
-        }
-        catch (ScopeException exception) {
+        } catch (ScopeException exception) {
             assertTrue(scope.isClosed());
-            assertThat(exception.getCause(), is(disposable2Exception));
+            assertThat(exception.getCause(), is(closeable3Exception));
             assertThat(exception.getSuppressed().length, is(1));
-            assertThat(exception.getSuppressed()[0], is(disposable1Exception));
-            assertTrue(nonThrowingDisposable.isDisposed());
+            assertThat(exception.getSuppressed()[0], is(closeable1Exception));
+            verify(closeable1, times(1)).close();
+            verify(closeable2, times(1)).close();
+            verify(closeable3, times(1)).close();
         }
     }
+
+    private interface Closeable {
+        void close() throws Exception;
+    }
+
 }
